fc7cbdd8a3f5c283fbbf2749472ee37e8f0fa228
Joanna Wiecek <jwiecek@onet.pl>
2017-08-16 09:53:06 +0200

    kantor project added

---
+++ kantor/app.js
@@ -0,0 +1,12 @@
+console.log("test");
+
+//pobieranie wartości z inputa
+var amount = document.getElementById('amount');
+var btn = document.getElementById('count');
+
+btn.addEventListener('click', function() {
+    var res = document.getElementById('result');
+    res.innerHTML = "Kwota w USD to: " + (parseFloat(amount.value) / 3.65).toFixed(2);
+    
+        //console.log(amount.value);
+});
